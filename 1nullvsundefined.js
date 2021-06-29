8853b6b0c04e7028a89fa68521c1abb27e10c37e
shubham singh <shubham.singh@truminds.com>
2021-06-29 13:43:19 +0530

    null vs undefined

---
+++ 1nullvsundefined.js
@@ -0,0 +1,27 @@
+let x;
+console.log(x);
+console.log(typeof x);
+
+// Undefined is a datatype in javascript.A variable having no values has undefined as value and its of type undefined.
+
+let y=null;
+console.log(y);
+console.log(typeof y);
+
+// Null is an object in javascript.A variable having null as value is of type object
+
+console.log(x==y);
+
+// Since both null and undefined means nothing as a value hence they both are equal in terms of value
+
+console.log(x===y);
+
+// Since undefined is a data type and null is of type object,they both have different datatypes
+
+
+/* 
+Null: It is the intentional absence of the value. It is one of the primitive values of JavaScript.
+
+Undefined: It means the value does not exist in the compiler. It is the global object.
+
+*/
